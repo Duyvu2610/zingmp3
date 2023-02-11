ab747fa4f01c5b464e251d7ca945ac1f4403071f
DuyVu <duyvu2612003@gmail.com>
2023-02-11 14:46:20 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 8888;
+
+// ZingMp3Router
+const ZingMp3Router = require("./routers/api/ZingRouter");
+app.use("/api", cors({ origin: process.env.URL_CLIENT }), ZingMp3Router);
+
+// Page Error
+app.get("*", (req, res) => {
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<");
+});
+
+app.listen(port, () => {
+  console.log(`Start server listen at http://localhost:${port}`);
+});
